5af1b4783b36250f73b288b80957aac841aabd90
Julia-mas <linniks2806@gmail.com>
2022-01-15 23:13:25 +0200

    Updated students

--- lms/settings.py
+++ lms/settings.py
@@ -60,6 +60,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     "debug_toolbar.middleware.DebugToolbarMiddleware",
+    'core.middlewares.SimpleMiddleware',
 ]
 
 ROOT_URLCONF = 'lms.urls'
--- students/views.py
+++ students/views.py
@@ -1,7 +1,7 @@
-
 from django.urls import reverse_lazy
 from django.views.generic import UpdateView, ListView, CreateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib import messages
 
 from .forms import StudentCreateForm
 from .models import Students
@@ -9,16 +9,24 @@ from .forms import StudentsFilter
 
 
 class StudentsListView(ListView):
+    paginate_by = 10
     model = Students
     template_name = 'students/list.html'
 
-    def get_queryset(self):
-        filter_students = StudentsFilter(
+    def get_filter(self):
+        return StudentsFilter(
             data=self.request.GET,
             queryset=self.model.objects.all().select_related('group', 'headman_group')
         )
 
-        return filter_students
+    def get_queryset(self):
+        return self.get_filter().qs
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['filter_forms'] = self.get_filter().form
+
+        return context
 
 
 class StudentCreateView(LoginRequiredMixin, CreateView):
@@ -27,6 +35,13 @@ class StudentCreateView(LoginRequiredMixin, CreateView):
     success_url = reverse_lazy('students:list')
     template_name = 'students/create.html'
 
+    def form_valid(self, form):
+        result = super().form_valid(form)
+        messages.success(self.request, 'Student {} successfully has been created'.format(
+            self.object))
+
+        return result
+
 
 class StudentUpdateView(LoginRequiredMixin, UpdateView):
     model = Students
@@ -34,7 +49,21 @@ class StudentUpdateView(LoginRequiredMixin, UpdateView):
     success_url = reverse_lazy('students:list')
     template_name = 'students/update.html'
 
+    def form_valid(self, form):
+        result = super().form_valid(form)
+        messages.success(self.request, 'Student {} successfully has been changed'.format(self.object))
+
+        return result
+
 
 class StudentDeleteView(LoginRequiredMixin, DeleteView):
     model = Students
-    success_url = reverse_lazy('student:list')
+    template_name = 'students/students_confirm_delete.html'
+    success_url = reverse_lazy('students:list')
+
+    def delete(self, request, *args, **kwargs):
+        res = super(StudentDeleteView, self).delete(request, *args, **kwargs)
+        messages.success(self.request, 'Student {} successfully removed'.format(
+            self.object))
+        return res
+
